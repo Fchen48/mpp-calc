63eb0552d3fdd925be9bb822df896b43db77777b
Mario Fridrichovsky <mario.fridrichovsky@wksimonsfeld.at>
2019-08-27 12:56:33 +0200

    release 1.0.0

---
+++ app.js
@@ -0,0 +1,68 @@
+const { app, BrowserWindow, ipcMain } = require("electron");
+const electronPug = require("electron-pug");
+const path = require("path");
+const url = require("url");
+
+let mainWindow;
+const width = 480;
+const height = 620;
+
+app.on("ready", () => {
+    setTimeout(() => {
+        const woptions = {
+            webPreferences: {
+                nodeIntegration: true
+            },
+            show: false,
+            frame: false,
+            transparent: true,
+            width,
+            minWidth: width,
+            maxWidth: width,
+            height,
+            minHeight: height,
+            maxHeight: height,
+            icon: path.join(__dirname, "images", "favicon.png")
+        };
+
+        electronPug({ pretty: true })
+        .then(() => {
+            // pug.on("error", console.error);
+
+            mainWindow = new BrowserWindow(woptions);
+
+            mainWindow.loadURL(url.format({
+                pathname: path.join(__dirname, "views", "index.pug"),
+                protocol: "file:",
+                slashes: true
+            }));
+
+            mainWindow.on("closed", () => {
+                app.quit();
+            });
+
+            mainWindow.once("ready-to-show", () => {
+                mainWindow.show();
+            });
+
+            mainWindow.on("maximize", () => console.log("full-screen"));
+
+            mainWindow.setFullScreenable(false);
+            mainWindow.setMaximizable(false);
+            mainWindow.setResizable(false);
+        })
+        .catch(console.error);
+
+    }, 100);
+});
+
+ipcMain.on("window:minimize", () => {
+    const window = BrowserWindow.getFocusedWindow();
+    if(window.isMinimized()) {
+        return window.restore();
+    }
+    return window.minimize();
+});
+
+process.on("uncaughtException", console.error);
+process.on("unhandledRejection", console.error);
